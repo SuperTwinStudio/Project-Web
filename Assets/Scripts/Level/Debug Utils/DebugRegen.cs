5a03204be02ba8da76e186dc5cc31902816cb5b5
ThatBit <58744468+ThatBit345@users.noreply.github.com>
2025-10-02 10:56:37 +0200

    Secret room test UI
    - Added a new panel to the generation test UI: Secret room generation

--- Assets/Scripts/Level/Debug Utils/DebugRegen.cs
+++ Assets/Scripts/Level/Debug Utils/DebugRegen.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using TMPro;
 using System;
+using UnityEngine.UI;
 
 public class DebugRegen : MonoBehaviour
 {
@@ -11,13 +12,14 @@ public class DebugRegen : MonoBehaviour
 	[SerializeField] private TMP_InputField m_SizeField;
 	[SerializeField] private TMP_InputField m_StepField;
 	[SerializeField] private TMP_InputField m_DoorField;
+	[Space]
+	[SerializeField] private GameObject m_SecretPanel;
+	[SerializeField] private TMP_InputField m_SecretField;
+	[SerializeField] private Toggle m_SecretGenerationToggle;
+	[SerializeField] private Toggle m_SecretDoorGenerationToggle;
 
 	void Start()
 	{
-		m_SizeField.text = m_DebugDefinition.MapSize.ToString();
-		m_StepField.text = m_DebugDefinition.MaxStep.ToString();
-		m_DoorField.text = m_DebugDefinition.BaseDoorChance.ToString();
-
 		// Copy room data from the donor
 		m_DebugDefinition.StartRoom = m_RoomDonor.StartRoom;
 		m_DebugDefinition.StandardRooms = m_RoomDonor.StandardRooms;
@@ -28,6 +30,23 @@ public class DebugRegen : MonoBehaviour
 		m_DebugDefinition.SecretRoomsHaveDoors = m_RoomDonor.SecretRoomsHaveDoors;
 		m_DebugDefinition.SecretRoomChance = m_RoomDonor.SecretRoomChance;
 		m_DebugDefinition.SecretRooms = m_RoomDonor.SecretRooms;
+
+		// Initialize fields
+		m_SizeField.text = m_DebugDefinition.MapSize.ToString();
+		m_StepField.text = m_DebugDefinition.MaxStep.ToString();
+		m_DoorField.text = m_DebugDefinition.BaseDoorChance.ToString();
+
+		// Initialize secret room panel
+		if (m_DebugDefinition.SecretRooms.Length == 0)
+		{
+			m_SecretPanel.SetActive(false);
+		}
+		else
+		{
+			m_SecretField.text = m_DebugDefinition.SecretRoomChance.ToString();
+			m_SecretGenerationToggle.isOn = m_DebugDefinition.GenerateSecretRooms;
+			m_SecretDoorGenerationToggle.isOn = m_DebugDefinition.SecretRoomsHaveDoors;
+		}
 	}
 
 	void Update()
@@ -71,5 +90,26 @@ public class DebugRegen : MonoBehaviour
 	public void Regen()
 	{
 		m_Generator.GenerateLevel(m_DebugDefinition);
+	}
+
+	public void ToggleSecretGeneration(bool _value)
+	{
+		m_DebugDefinition.GenerateSecretRooms = _value;
+	}
+
+	public void ToggleDoorGeneration(bool _value)
+	{
+		m_DebugDefinition.SecretRoomsHaveDoors = _value;
+	}
+
+	public void SetSecretChance()
+	{
+		m_DebugDefinition.SecretRoomChance = Int32.Parse(m_SecretField.text);
+	}
+
+	public void ResetSecretChance()
+	{
+		m_DebugDefinition.SecretRoomChance = 25;
+		m_SecretField.text = "25";
 	}
 }
