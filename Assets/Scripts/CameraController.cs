a602ff3ddcef9bdc831dd590ea2b49e4fc1ee66c
racurrandom <racurrandom@gmail.com>
2025-10-09 12:26:54 +0200

    blockout and autofocus

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class CameraController : MonoBehaviour {
 
@@ -11,20 +13,30 @@ public class CameraController : MonoBehaviour {
     //Components
     [Header("Components")]
     [SerializeField] private Camera _camera;
+    [SerializeField] private Transform focusTarget;
+    [SerializeField] private Volume postproVolume;
+    
 
     private Transform cameraTransform;
 
     public Transform Follow { get; set; }
     public Camera Camera => _camera;
 
+    private DepthOfField DOF;
+
 
     //State
     private void Start() {
         cameraTransform = transform;    
+        if (postproVolume.profile.TryGet(out DepthOfField _DOF))
+        {
+            DOF = _DOF;
+        }
     }
 
     private void LateUpdate() {
         cameraTransform.position = Follow.position;
+        DOF.focusDistance.value = Vector3.Distance(cameraTransform.position, focusTarget.position);
     }
 
 }
