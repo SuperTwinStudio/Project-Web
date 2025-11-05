cd16c03e6e804eed4b34b26cb5f91163d48081dc
ThatBit <58744468+ThatBit345@users.noreply.github.com>
2025-11-05 15:36:23 +0100

    Finished "Orbital Buddy"

--- Assets/Scripts/Items/Passive Items/Support/OrbitalBuddy.cs
+++ Assets/Scripts/Items/Passive Items/Support/OrbitalBuddy.cs
@@ -4,38 +4,63 @@ using UnityEngine;
 public class OrbitalBuddy : MonoBehaviour
 {
     [SerializeField] private float m_RotationSpeed = 1;
+    [SerializeField] private float m_CooldownMax = 1;
+    [SerializeField] private float m_DetectionMax = 1;
+    private Player m_Player;
 
-    private List<Character> m_EnemyQueue;
+    private Character m_TargetEnemy;
     private int m_Damage;
+    private float m_Cooldown;
+    private float m_DetectionCooldown;
+
+    private float m_DetectionRadius;
 
     void Start()
     {
-        m_EnemyQueue = new List<Character>();
+        m_TargetEnemy = null;
+        m_Player = Game.Current.Level.Player;
+        m_DetectionRadius = GetComponent<SphereCollider>().radius;
     }
 
     void Update()
     {
+        transform.position = m_Player.transform.position;
+
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + (Vector3.up * m_RotationSpeed));
+
+        if (m_Cooldown > 0)
+        {
+            m_Cooldown -= Time.deltaTime;
+        }
+        else if (m_TargetEnemy != null)
+        {
+            m_Cooldown = m_CooldownMax;
+            m_TargetEnemy.Damage(m_Damage, m_Player.GetComponent<Player>(), DamageType.Ranged);
+        }
+
+        if (m_DetectionCooldown > 0)
+        { 
+            m_DetectionCooldown -= Time.deltaTime; 
+        }
+        else
+        {
+            m_DetectionCooldown = m_DetectionMax;
+
+            Collider[] colliders = Physics.OverlapSphere(transform.position, m_DetectionRadius);
+            foreach (Collider collider in colliders)
+            {
+                if (collider.CompareTag("Enemy"))
+                {
+                    Debug.Log($"Added {collider.name}");
+                    m_TargetEnemy = collider.GetComponent<Character>();
+                }
+            }
+        }
     }
 
     public void UpdateItemCount(int itemCount)
     {
-        m_Damage = 10 * (50 + (25 * itemCount)) / 100;
-    }
-
-    public void OnTriggerEnter(Collider other)
-    {
-        if (other.GetComponent<Character>() != null && !other.CompareTag("Player"))
-        {
-            m_EnemyQueue.Add(other.GetComponent<Character>());
-        }
-    }
-
-    public void OnTriggerExit(Collider other)
-    {
-        if (other.GetComponent<Character>() != null && !other.CompareTag("Player"))
-        {
-            m_EnemyQueue.Remove(other.GetComponent<Character>());
-        }
+        // 50% of base damage + 25% for each extra item
+        m_Damage = (30 * (50 + (25 * (itemCount - 1)))) / 100;
     }
 }
