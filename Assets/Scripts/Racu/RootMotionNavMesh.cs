0b7ba1ec149e228bfe24523ea214df31bd651c15
racurrandom <racurrandom@gmail.com>
2025-11-29 13:59:13 +0100

    Knight rootmotion movement

---
+++ Assets/Scripts/Racu/RootMotionNavMesh.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RootMotionNavMesh : MonoBehaviour {
+        
+    public bool ApplyRootmotion = true;
+    [SerializeField] NavMeshAgent navMeshAgent;
+    [SerializeField] Animator animator;
+
+    private Transform t;
+
+    private void Start() {
+        t = navMeshAgent.gameObject.transform;
+    }
+
+    private void OnAnimatorMove()
+    {
+        if (ApplyRootmotion) Move();
+    }
+
+    void Move() {
+        Vector3 newPosition = t.position + animator.deltaPosition;
+        Vector3 vel = newPosition - t.position;
+        navMeshAgent.Move(vel);
+    }
+    
+}
