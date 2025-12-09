26fc3672270d679ea0bf128f648191563c3e135c
Pablo <58744468+ThatBit345@users.noreply.github.com>
2025-12-09 14:03:24 +0100

    Fixed Hidden Blade item

--- Assets/Scripts/Items/Passive Items/PIConcealedBlade.cs
+++ Assets/Scripts/Items/Passive Items/PIConcealedBlade.cs
@@ -9,13 +9,15 @@ public class PIConcealedBlade : PassiveItem
     {
         if (GetScaledChance(itemCount, 50, 5))
         {
+            Vector3 verticalOffset = Vector3.up * 0.5f;
+
             GameObject leftBlade = Instantiate(m_Blade);
             leftBlade.transform.rotation = Quaternion.Euler(player.Model.transform.rotation.eulerAngles - (Vector3.up * -m_BladeAngle));
-            leftBlade.transform.position = player.transform.position + leftBlade.transform.forward * 0.5f;
+            leftBlade.transform.position = player.transform.position + leftBlade.transform.forward * 0.5f + verticalOffset;
 
             GameObject rightBlade = Instantiate(m_Blade);
             rightBlade.transform.rotation = Quaternion.Euler(player.Model.transform.rotation.eulerAngles - (Vector3.up * m_BladeAngle));
-            rightBlade.transform.position = player.transform.position + rightBlade.transform.forward * 0.5f;
+            rightBlade.transform.position = player.transform.position + rightBlade.transform.forward * 0.5f + verticalOffset;
         }
     }
 }
--- Assets/Scripts/Items/Passive Items/Support/Blade.cs
+++ Assets/Scripts/Items/Passive Items/Support/Blade.cs
@@ -4,6 +4,13 @@ public class Blade : MonoBehaviour
 {
     [SerializeField] private float m_Speed;
     [SerializeField] private float m_Lifetime;
+    private Rigidbody m_Rigidbody => GetComponent<Rigidbody>();
+
+    private void Start()
+    {
+        //Apply velocity
+        m_Rigidbody.linearVelocity = m_Speed * transform.forward;
+    }
 
     // Update is called once per frame
     void Update()
@@ -11,7 +18,29 @@ public class Blade : MonoBehaviour
         m_Lifetime -= Time.deltaTime;
 
         if(m_Lifetime <= 0) Destroy(gameObject);
+    }
 
-        transform.position += transform.forward * m_Speed* Time.deltaTime;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Something");
+
+        if (other.isTrigger) return;
+
+        //Check if damageable
+        if (other.TryGetComponent(out IDamageable damageable)) 
+        {
+            if (other.CompareTag("Player")) return;
+
+            Debug.Log("ENEMY!");
+
+            //Deal damage
+            damageable.Damage(15, DamageType.Ranged, Game.Current.Level.Player);
+
+            Destroy(gameObject);
+        } 
+        else 
+        {
+            Destroy(gameObject);
+        }
     }
 }
